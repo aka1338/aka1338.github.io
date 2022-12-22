3c118df0d1e5377b183f7f6cb60f75b6846b7ef4
John Paul Apolinar <john.apolinar@gmail.com>
2022-12-22 14:32:47 -0800

    Experience and Home first pass

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,8 +1,55 @@
 import React from 'react'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@mui/icons-material/School";
+import "../styles/Experience.css";
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2012 - 2016"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title"> Abraham Lincoln High School, San Francisco, CA</h3>
+          <p> High School Diploma </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2017 - 2019"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title"> City College, San Francisco, CA </h3>
+
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2019 - 2022"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title"> San Francisco State University, San Francisco, CA </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            Bachelor's Degree
+          </h4>
+
+          <p>Computer Science</p>
+        </VerticalTimelineElement>
+
+
+      </VerticalTimeline>
+    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import LinkedInIcon from "@mui/icons-material/LinkedIn"
+import EmailIcon from "@mui/icons-material/Email"
+import GithubIcon from "@mui/icons-material/GitHub"
+import "../styles/Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="about">
+        <h2>Hi, my name is John Paul </h2>
+        <div className="prompt">
+          <p>A software developer with a passion for learning and creating.</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+      <div className="skills"></div>
+      <h1>Skills</h1>
+      <ol className="list">
+        <li className="item">
+          <h2>Front-End</h2>
+          <span>
+            ReactJS, CSS, HTML
+          </span>
+        </li>
+        <li className="item">
+          <h2>Back-End</h2>
+          <span>
+            NodeJS, MySQL, SQL, PostGres, PHP
+          </span>
+        </li>
+        <li className="item">
+          <h2>Languages</h2>
+          <span>
+            Javascript, Java, C#, C, C++
+          </span>
+        </li>
+      </ol>
+    </div>
   );
 }
 
